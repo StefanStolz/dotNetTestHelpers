d5e4fa1c1c8ef360120296309648445503e2f2d9
Stefan Stolz <stefan.stolz@gmx.de>
2025-05-14 21:41:58 +0200

    StringComparerBuilder

---
+++ src/TestHelpers.Tests/StringComparerBuilderTests.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+
+namespace StefanStolz.TestHelpers.Tests;
+
+[TestFixture]
+[TestOf(typeof(StringComparerBuilder))]
+public class StringComparerBuilderTests
+{
+
+    [Test]
+    public void BuildComparerIgnoringLineEndings()
+    {
+        var input1 = "one\ntwo\nthree";
+        var input2 = "one\r\ntwo\r\nthree";
+
+        var sut = new StringComparerBuilder().IgnoreLineEndings().Build();
+
+        var result = sut.Equals(input1, input2);
+
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public void TrimLines()
+    {
+        var input1 = "  one\ntwo  ";
+        var input2 = "one\ntwo";
+
+        var sut = new StringComparerBuilder().TrimLines().Build();
+
+        var result = sut.Equals(input1, input2);
+
+        result.Should().BeTrue();
+    }
+}
---
+++ src/TestHelpers/StringComparerBuilder.cs
@@ -0,0 +1,124 @@
+using System.Runtime.InteropServices;
+
+namespace StefanStolz.TestHelpers;
+
+public sealed class StringComparerBuilder
+{
+    private bool trimLines = false;
+    private bool ignoreLineEndings = false;
+
+    public static StringComparerBuilder Create()
+    {
+        return new StringComparerBuilder();
+    }
+
+
+    public StringComparerBuilder IgnoreLineEndings()
+    {
+        this.ignoreLineEndings = true;
+        return this;
+    }
+
+    public StringComparerBuilder TrimLines()
+    {
+        this.trimLines = true;
+        return this;
+    }
+
+    public IEqualityComparer<string> Build()
+    {
+        var preProcessors = new List<IStringPreprocessor>();
+
+        if (this.trimLines)
+        {
+            preProcessors.Add(new TrimLinesStringProcessor());
+        }
+
+        if (this.ignoreLineEndings)
+        {
+            preProcessors.Add(new IgnoreLineEndingsStringPreprocessor());
+        }
+
+        return new Comparer(preProcessors);
+    }
+
+    private sealed class TrimLinesStringProcessor : IStringPreprocessor
+    {
+        public string Preprocess(string input)
+        {
+            using var stringReader = new StringReader(input);
+            var lines = new List<string>();
+            while (stringReader.ReadLine() is { } line)
+            {
+                lines.Add(line.Trim());
+            }
+
+            return string.Join(Environment.NewLine, lines);
+        }
+    }
+
+    private sealed class IgnoreLineEndingsStringPreprocessor : IStringPreprocessor
+    {
+        public string Preprocess(string input)
+        {
+            using var stringReader = new StringReader(input);
+            string? line;
+            var lines = new List<string>();
+            while ((line = stringReader.ReadLine()) is not null)
+            {
+                lines.Add(line);
+            }
+
+            return string.Join(Environment.NewLine, lines);
+        }
+    }
+
+    private interface IStringPreprocessor
+    {
+        string Preprocess(string input);
+    }
+
+    private sealed class Comparer : IEqualityComparer<string>
+    {
+        private readonly IReadOnlyList<IStringPreprocessor> preprocessors;
+
+        public Comparer(IEnumerable<IStringPreprocessor> preprocessors)
+        {
+            this.preprocessors = preprocessors.ToList().AsReadOnly();
+        }
+
+        public bool Equals(string? x, string? y)
+        {
+            switch ((x, y))
+            {
+                case (null, null):
+                    return true;
+                case (_, null):
+                case (null, _):
+                    return false;
+                default:
+                {
+                    foreach (IStringPreprocessor stringPreprocessor in this.preprocessors)
+                    {
+                        x = stringPreprocessor.Preprocess(x);
+                        y = stringPreprocessor.Preprocess(y);
+                    }
+
+                    return x.Equals(y);
+                }
+            }
+        }
+
+        public int GetHashCode(string obj)
+        {
+            var value = obj;
+
+            foreach (IStringPreprocessor stringPreprocessor in this.preprocessors)
+            {
+                value = stringPreprocessor.Preprocess(value);
+            }
+
+            return value.GetHashCode();
+        }
+    }
+}
